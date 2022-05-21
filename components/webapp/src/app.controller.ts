d21f66418e531dc0360c42b1e165f551930a910f
Feroz Ahmmed <froghramar@gmail.com>
2022-05-21 12:20:39 +0600

    add hbs view engine

--- components/webapp/src/app.controller.ts
+++ components/webapp/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+
+class IndexResponse {
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getHello(): IndexResponse {
+    return { message: this.appService.getHello() };
   }
 }
